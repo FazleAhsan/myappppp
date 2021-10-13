464ec7baa31e08327ec5f8cc4aa6fcdb37d2e583
Eng Fazle Ahsan <fazleahsan2222@gmail.com>
2021-10-13 17:51:13 +0600

    init

---
+++ lib/AtakeZ.dart
@@ -0,0 +1,23 @@
+import 'dart:async';
+
+import 'package:flutter/material.dart';
+
+void main()
+{
+
+  runApp( const MyApp ());
+
+}
+
+class  extends StatelessWidget {
+  const ({Key? key}) : super(key: key);
+@override
+  noSuchMethod(Invocation invocation) {
+    // TODO: implement noSuchMethod
+    return super.noSuchMethod(invocation);
+  }
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
---
+++ lib/p1.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+
+void main()
+{
+
+  runApp( const MyApp ());
+
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp ({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp( debugShowCheckedModeBanner: false,
+
+      home: Scaffold(
+        body: Center(
+          child: (
+              Container(
+                child:Center(child: Text("Dhaka",)),
+                height:200 ,
+                width:200 ,
+                color:Colors.green,
+              )
+          ),
+        ),
+      ),
+    );
+  }
+}
